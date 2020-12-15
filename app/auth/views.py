2fad1262b7b9ac5a9a4f7b8999a0a0eb73d0118c
abdisamad100 <abdisamadcade004@gmail.com>
2020-12-15 09:46:18 +0300

    added email.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from flask import render_template,redirect,url_for, flash,request
 from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
-
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -21,6 +21,18 @@ def login():
     return render_template('auth/login.html',login_form = login_form,title=title)    
 
 
+# @auth.route('/register',methods = ["GET","POST"])
+# def register():
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+#         db.session.add(user)
+#         db.session.commit()
+#         return redirect(url_for('auth.login'))
+#         title = "New Account"
+#     return render_template('auth/register.html',registration_form = form)
+
+
 @auth.route('/register',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
@@ -28,9 +40,12 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
+    return render_template('auth/register.html',registration_form = form)    
 
 @auth.route('/logout')
 @login_required
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
- export SECRET_KEY='K7229Y7222Y'
+export SECRET_KEY='K7229Y7222Y'
+export MAIL_USERNAME='abdisamadcade004@gmail.com'
+export MAIL_PASSWORD='MUNIR004'
 
 python3.6 manage.py server
