97e0a06c780632a3ae1cd3a8181d78fd55d8b1cc
abdisamad100 <abdisamadcade004@gmail.com>
2020-12-15 11:04:11 +0300

    modified blogs

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask_login import login_required
+from flask_login import login_required,current_user
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import User,Blog
